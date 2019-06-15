3e231ad1cf9430372e60b85626f5bdb1bccbf0c6
Tevvin <Tevvin7395@icloud.com>
2019-06-15 12:37:27 -0400

    Installed bcrypt and implemented user registration

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
 
 const { authenticate } = require('../auth/authenticate');
+//const Users = require('./users-model.js');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,6 +12,17 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+
+  Users
+    .add(user).then(users => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'User not registed' });
+    })
 }
 
 function login(req, res) {
