8c45b5d242b52a0d26e920d423331a876c9c2dae
Tevvin <Tevvin7395@icloud.com>
2019-06-15 12:58:15 -0400

    added login using bcrypt and jsonwebtoken

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const { authenticate } = require('../auth/authenticate');
-//const Users = require('./users-model.js');
+const { authenticate, jwtKey } = require('../auth/authenticate');
+const Users = require('./users-model.js');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -27,6 +28,35 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({ message: `Welcome ${user.username}!`, token, });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+}
+
+function generateToken(user) {
+  const payload = {
+    userId: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1h',
+  };
+
+  return jwt.sign(payload, jwtKey, options);
 }
 
 function getJokes(req, res) {
